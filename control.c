dc25b034baf80e6ca8e905a5ea71d6dbffa45fc1
Omar Polo <op@omarpolo.com>
2021-07-22 14:38:15 +0200

    get queue.h via compat.h

--- control.c
+++ control.c
@@ -14,8 +14,9 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include "compat.h"
+
 #include <sys/types.h>
-#include <sys/queue.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/uio.h>
