7d1b6186efe32d7d71ed97be1a290913a8ce904f
Omar Polo <op@omarpolo.com>
2021-08-07 23:14:02 +0200

    add `vargs' to the list of keywords

--- contrib/9ps-mode.el
+++ contrib/9ps-mode.el
@@ -27,7 +27,7 @@
 
 (defconst 9ps--font-lock-defaults
   (let ((keywords '("assert" "const" "dir" "include" "proc" "should-fail"
-                    "testing"))
+                    "testing" "vargs"))
         (types '("str" "u8" "u16" "u32")))
     `(((,(rx-to-string `(: (or ,@keywords))) 0 font-lock-keyword-face)
        ("\\([[:word:]]+\\)\s*(" 1 font-lock-function-name-face)
