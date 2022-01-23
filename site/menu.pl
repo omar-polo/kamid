c8477a004f79c72afc1a7f965b169cf639d0a1f9
Omar Polo <op@omarpolo.com>
2022-01-23 21:11:55 +0000

    site: add menu on the www site

---
+++ site/menu.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use v5.10;
+use strict;
+use warnings;
+
+my $page = shift or die 'missing page';
+my @pages = ();
+
+while (<>) {
+	chomp;
+	@pages = (@pages, $_);
+}
+
+my $did = 0;
+for (@pages) {
+	if (!$did) {
+		$did = 1;
+	} else {
+		print "| ";
+	}
+
+	my ($href, $text) = m/^([^\s]*)\s*(.*)$/;
+	if ($href eq $page) {
+		print "<strong>$text</strong> ";
+	} else {
+		print "<a href='$href'>$text</a> ";
+	}
+}
+
+say "";
